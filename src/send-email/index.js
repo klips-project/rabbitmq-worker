1a2a20653975dc515b486fc5978d505249f8bd28
Michael Holthausen <mholthausen@terrestris.de>
2021-07-07 11:36:58 +0200

    Adds new worker for sending emails

---
+++ src/send-email/index.js
@@ -0,0 +1,68 @@
+import nodemailer from 'nodemailer';
+import { log, initialize, errorAndExit } from '../workerTemplate.js';
+
+const workerQueue = process.env.WORKERQUEUE;
+const resultQueue = process.env.RESULTSQUEUE;
+const rabbitHost = process.env.RABBITHOST;
+const mailHost = process.env.MAILHOST;
+const mailPort = process.env.MAILPORT;
+const secure = process.env.SECURE === 'true' ? true : false;
+const authUser = process.env.AUTHUSER;
+const authPass = process.env.AUTHPASS;
+const fromSenderName = process.env.FROMSENDERNAME;
+const fromSenderEmail = process.env.FROMSENDEREMAIL;
+
+/**
+ * Sends an email to an address specified in the job_sender workflow via smtp
+ * and uses the server specified in the docker-compose file
+ * There you have to enter the access data for a valid account on an existing
+ * email server.
+ * @param {Object} workerJob The job object
+ * @param {Array} inputs The inputs for this process
+ * @example
+ *   {
+       "type": "send-email",
+       "inputs": [
+         "to@recipient.org",
+         "subject",
+         "content"
+       ]
+     }
+ */
+const sendEmail = async (workerJob, inputs) => {
+  const recipientEmail = inputs[0];
+  const subject = inputs[1] || 'No subject';
+  const text = inputs[2] || 'No content';
+  const transporter = nodemailer.createTransport({
+    host: mailHost,
+    port: mailPort,
+    secure: secure, // true for 465, false for other ports
+    auth: {
+      user: authUser,
+      pass: authPass
+    }
+  });
+
+  transporter.verify(async function (error) {
+    if (error) {
+      log(error);
+    } else {
+      await transporter
+        .sendMail({
+          from: `"${fromSenderName}" <${fromSenderEmail}>`,
+          to: recipientEmail,
+          subject,
+          text
+        })
+        .catch(errorAndExit);
+    }
+  });
+
+  log(`Sending mail to ${recipientEmail} …`);
+
+  workerJob.status = 'success';
+  workerJob.outputs = [];
+};
+
+// Initialize and start the worker process
+initialize(rabbitHost, workerQueue, resultQueue, sendEmail);
