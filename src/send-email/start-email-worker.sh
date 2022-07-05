8e319748328bda398c3ef0af350b5be85308e8f6
Jakob Miksch <jakob@meggsimum.de>
2022-07-05 11:23:20 +0200

    Update start-email-worker.sh

--- src/send-email/start-email-worker.sh
+++ src/send-email/start-email-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # command called externally to be able to use the variables
-./wait-for.sh "${RABBITHOST}:5672" -- node worker/index.js ${MAILHOST} ${MAILPORT} ${SECURE} ${AUTHUSER} ${AUTHPASS} ${FROMSENDERNAME} ${FROMSENDEREMAIL}"
+./wait-for.sh "${RABBITHOST}:5672" -- node worker/index.js ${MAILHOST} ${MAILPORT} ${SECURE} ${AUTHUSER} ${AUTHPASS} ${FROMSENDERNAME} ${FROMSENDEREMAIL}
