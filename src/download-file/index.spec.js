d424347f67af235fa5015c657488cc003bd2e4bd
Andreas Schmitz <schmitz@terrestris.de>
2022-09-07 13:07:59 +0200

    Add tests for download-file

--- src/download-file/index.spec.js
+++ src/download-file/index.spec.js
@@ -1,6 +1,16 @@
 process.env.RABBITHOST = '46.4.8.29';
-import index from './index';
+import download from './index';
 
 test('test if download-file can be loaded', () => {
-    expect(index).toBeDefined();
+    expect(download).toBeDefined();
+});
+
+test('test if download-file can be called', async () => {
+    const job = {};
+    await download(job, [
+        'https://raw.githubusercontent.com/klips-project/rabbitmq-worker/main/README.md',
+        'test'
+    ]);
+    expect(job.outputs).toBeDefined();
+    expect(job.outputs.length).toBe(1);
 });
