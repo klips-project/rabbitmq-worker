097023cca05f94abb2ad941804d801ccb95badf4
blitza@terrrestris.de <blitza@terrestris.de>
2022-10-26 16:23:06 +0200

    Readd test

--- src/geotiff-validator/index.spec.js
+++ src/geotiff-validator/index.spec.js
@@ -1,21 +1,20 @@
 process.env.RABBITHOST = '46.4.8.29';
 import validate from './index';
-// import download from '../download-file/index';
+import download from '../download-file/index';
 
 test('test if geotiff-validate can be loaded', () => {
     expect(validate).toBeDefined();
 });
 
-// TODO FIX TEST
-// test('test if geotiff-validate can be called', async () => {
-//     let job = {};
-//     const downloadPath = '/tmp/test';
-//     await download(job, [
-//         'https://raw.githubusercontent.com/klips-project/klips-sdi/main/mocked-webspace/sample_germany_small.tif',
-//         downloadPath
-//     ]);
-//     job = {};
-//     await validate(job, [downloadPath]);
-//     expect(job.outputs).toBeDefined();
-//     expect(job.outputs.length).toBe(1);
-// });
+test('test if geotiff-validate can be called', async () => {
+    let job = {};
+    const downloadPath = '/tmp/test';
+    await download(job, [
+        'https://raw.githubusercontent.com/klips-project/klips-sdi/main/mocked-webspace/sample_germany_small.tif',
+        downloadPath
+    ]);
+    job = {};
+    await validate(job, [downloadPath]);
+    expect(job.outputs).toBeDefined();
+    expect(job.outputs.length).toBe(1);
+});
