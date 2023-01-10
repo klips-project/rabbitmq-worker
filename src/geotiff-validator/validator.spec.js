184e9905d242af24a8b508db52ce116945e419ac
Jakob Miksch <jakob@meggsimum.de>
2023-01-10 13:43:14 +0100

    Refactor test and bandValidation

--- src/geotiff-validator/validator.spec.js
+++ src/geotiff-validator/validator.spec.js
@@ -1,45 +1,120 @@
 import { GeotiffValidator, validateFilesize, validateBands, validateDataType, validateExtent, validateProjection } from './validator.js';
 import gdal from 'gdal-async';
 
-test('Check if GeotiffValidator is defined', () => {
-    expect(GeotiffValidator).toBeDefined();
-});
-
-test('Check if validateFilesize is defined', () => {
-    expect(validateFilesize).toBeDefined();
-});
-
-test('Check if validateBands is defined', () => {
-    expect(validateBands).toBeDefined();
-});
-
-test('Check if validateDataType is defined', () => {
-    expect(validateDataType).toBeDefined();
-});
-
-test('Check if validateExtent is defined', () => {
-    expect(validateExtent).toBeDefined();
-});
-
-test('Check if validateProjection is defined', () => {
-    expect(validateProjection).toBeDefined();
-});
-
 const path = 'src/geotiff-validator/sample_data/sample.tif';
 
-test('file size validation', () => {
-    let result = validateFilesize(path, 33000, 34000);
-    expect(result.valid).toBe(true);
+describe('GeotiffValidator', () => {
+    it('returns true if GeoTIFF is valid', async () => {
+       const config =  {
+            extent: {
+               allowedExtent: [
+                    [
+                       1,
+                       1
+                    ],
+                    [
+                        89,
+                        89
+                    ]
+                ]
+            },
+            projection: {
+                allowedEPSGCodes: [
+                    4326,
+                ]
+            },
+            dataType: {
+                allowedDataTypes: [
+                    "Byte",
+                    "Float32"
+                ]
+            },
+            fileSize: {
+                minFileSize: 1000,
+                maxFileSize: 10000000
+            },
+            bands: {
+                expectedCount: 4
+            }
+        }
+        const geotiffValidator = new GeotiffValidator(config);
+        const result = await geotiffValidator.performValidation(path, ['extent', 'projection', 'dataType', 'fileSize', 'bands'])
+        const allStepsAreValid = result.every(stepResult => stepResult.valid)
+        expect(allStepsAreValid).toBe(true);
+    });
+});
 
-    result = validateFilesize(path, 1, 2);
-    expect(result.valid).toBe(false);
-})
 
-test('data type validation', async () => {
-    const dataset = await gdal.openAsync(path);
-    let result = await validateDataType(dataset, ['Byte']);
-    expect(result.valid).toBe(true);
+describe('validateFilesize', () => {
+    it('returns true if size is in valid range', () => {
+        const result = validateFilesize(path, 33000, 34000);
+        expect(result.valid).toBe(true);
+    });
+    it('returns false if size is outside valid range', () => {
+        const result = validateFilesize(path, 1, 2);
+        expect(result.valid).toBe(false);
+        expect(result.info).toBeDefined();
+    });
+});
 
-    result = await validateDataType(dataset, ['Float32']);
-    expect(result.valid).toBe(false);
+describe('validateProjection', () => {
+    it('returns true on valid projection', async () => {
+        const dataset = await gdal.openAsync(path);
+        const result = await validateProjection(dataset, [4326, 3035]);
+        expect(result.valid).toBe(true);
+    });
+
+    it('returns false on invalid projection', async () => {
+        const dataset = await gdal.openAsync(path);
+        let result = await validateProjection(dataset, [3857]);
+        expect(result.valid).toBe(false);
+        expect(result.info).toBeDefined();
+    });
+});
+
+describe('validateDataType', () => {
+    it('returns true on valid datatype', async () => {
+        const dataset = await gdal.openAsync(path);
+        const result = await validateDataType(dataset, ['Byte']);
+        expect(result.valid).toBe(true);
+    });
+
+    it('returns false on invalid datatype', async () => {
+        const dataset = await gdal.openAsync(path);
+        const result = await validateDataType(dataset, ['Float32']);
+        expect(result.valid).toBe(false);
+        expect(result.info).toBeDefined();
+    });
+});
+
+describe('validateBands', () => {
+    it('returns true on valid band count', async () => {
+        const dataset = await gdal.openAsync(path);
+        const result = await validateBands(dataset, 4);
+        expect(result.valid).toBe(true);
+    });
+
+    it('returns false on invalid band count', async () => {
+        const dataset = await gdal.openAsync(path);
+        const result = await validateBands(dataset, 1);
+        expect(result.valid).toBe(false);
+        expect(result.info).toBeDefined();
+    });
+});
+
+describe('validate Extent', () => {
+    it('returns true on valid extent', async () => {
+        const dataset = await gdal.openAsync(path);
+        const validExtent = [1, 1, 89, 89];
+        const result = await validateExtent(dataset, validExtent);
+        expect(result.valid).toBe(true);
+    });
+
+    it('returns false on invalid extent', async () => {
+        const dataset = await gdal.openAsync(path);
+        const invalidExtent = [2.2, 39.1, 3.5, 39.9];
+        const result = await validateExtent(dataset, invalidExtent);
+        expect(result.valid).toBe(false);
+        expect(result.info).toBeDefined();
+    });
 });
