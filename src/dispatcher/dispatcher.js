f21f5b41bc425fdfeb6f7037383f12e73cb8dc73
Jakob Miksch <jakob@meggsimum.de>
2023-01-26 16:35:49 +0100

    Refactor dispatcher

--- src/dispatcher/dispatcher.js
+++ src/dispatcher/dispatcher.js
@@ -1,17 +1,10 @@
 import amqp from 'amqplib';
-import { log } from '../workerTemplate.js';
 import { randomUUID } from 'crypto';
-
-const workerQueue = process.env.WORKERQUEUE;
-const resultQueue = process.env.RESULTSQUEUE;
-const rabbitHost = process.env.RABBITHOST;
-const rabbitUser = process.env.RABBITUSER;
-const rabbitPass = process.env.RABBITPASS;
-const rabbitHeartbeat = process.env.RABBITHEARTBEAT || 60;
-
-let channel;
+import { log } from '../workerTemplate.js';
 
 /**
+ * Dispatcher that handles communication between all workers via RabbitMQ.
+ *
  * Listen for a job message and trigger the different involved workers.
  * The entries in the job array are handled sequential, starting from top. The `type` of
  * a worker configuration is used as the queue name to push the job and its configuration to.
@@ -46,139 +39,179 @@ let channel;
     ]
   }
  */
-const dispatcher = async () => {
-  const connection = await amqp.connect({
-    hostname: rabbitHost,
-    username: rabbitUser,
-    password: rabbitPass,
-    heartbeat: rabbitHeartbeat
-  });
+export class Dispatcher {
+  /**
+   * Create a dispatcher instance.
+   *
+   * @param {String} workerQueue The name of the worker queue
+   * @param {String} resultQueue The name of the result queue
+   * @param {Object} rabbitConf 'amqplib connection object (see https://amqp-node.github.io/amqplib/channel_api.html#connect)
+   */
+  constructor(workerQueue, resultQueue, rabbitConf) {
+    this.rabbitConf = rabbitConf;
+    this.workerQueue = workerQueue;
+    this.resultQueue = resultQueue;
 
-  channel = await connection.createChannel();
+    this.connection = undefined;
+    this.channel = undefined;
+  }
 
-  channel.assertExchange('DeadLetterExchange', 'fanout', {
-    durable: true,
-    autoDelete: false
-  });
+  /**
+   * Init the dispatching process.
+   */
+  async init() {
+    this.connection = await amqp.connect(this.rabbitConf);
 
-  channel.assertQueue('DeadLetterQueue', { durable: true });
+    this.channel = await this.connection.createChannel();
 
-  channel.bindQueue('DeadLetterQueue', 'DeadLetterExchange', '');
+    this.channel.assertExchange('DeadLetterExchange', 'fanout', {
+      durable: true,
+      autoDelete: false
+    });
 
-  channel.assertQueue(workerQueue, {
-    durable: true,
-    arguments: {
-      'x-dead-letter-exchange': 'DeadLetterExchange'
-    }
-  });
-  channel.assertQueue(resultQueue, {
-    durable: true
-  });
+    this.channel.assertQueue('DeadLetterQueue', { durable: true });
 
-  log(
-    `Dispatcher waiting for messages in ` + `${workerQueue} and ${resultQueue}.`
-  );
+    this.channel.bindQueue('DeadLetterQueue', 'DeadLetterExchange', '');
 
-  channel.consume(workerQueue, handleNextTask, {
-    noAck: false
-  });
+    this.channel.assertQueue(this.workerQueue, {
+      durable: true,
+      arguments: {
+        'x-dead-letter-exchange': 'DeadLetterExchange'
+      }
+    });
+    this.channel.assertQueue(this.resultQueue, {
+      durable: true
+    });
 
-  channel.consume(resultQueue, handleResults, {
-    noAck: false
-  });
-};
-
-/**
- * Determine the next task from job list and sends it to worker queue.
- * If there is no task left, finish the execution with success.
- * If an error is detected in a result message, we `nack` the job message which
- * then will be forwarded to the dead letter exchange.
- * @param {Object} msg The message
- */
-const handleNextTask = (msg) => {
-  let nextTaskEntry;
-
-  try {
-    const job = JSON.parse(msg.content.toString());
-    const chain = job.job;
-    log('Received a job configuration ...');
-
-    if (job.error) {
-      throw job.error;
-    }
-
-    // validate
-    if (!chain || chain.length < 1) {
-      throw 'Invalid arguments given' + job;
-    }
-
-    // create unique ID if necessary (on first run)
-    if (!job.id) {
-      job.id = randomUUID();
-    }
-
-    // find next task that has not run yet (status is not 'success')
-    nextTaskEntry = chain.find(
-      (task) => !(task.status && task.status === 'success')
+    log(
+      `Dispatcher waiting for messages in ${this.workerQueue} and ${this.resultQueue}.`
     );
 
-    if (nextTaskEntry) {
-      job.nextTask = {
-        task: nextTaskEntry,
-        idx: chain.findIndex((el) => el.id === nextTaskEntry.id)
-      };
-      log(`Sending the next task to queue ${nextTaskEntry.type} ...`);
-      channel.sendToQueue(
-        nextTaskEntry.type,
-        Buffer.from(JSON.stringify({ content: job })),
-        {
-          persistent: true
-        }
-      );
-    } else {
-      // overall job success
-      job.status = 'success';
-      log('Job finished successfully: \n' + JSON.stringify(job, null, 2));
-    }
-    channel.ack(msg);
-  } catch (e) {
-    log('Processing failed for task. ' + e);
-    // send to dead letter exchange
-    channel.nack(msg, false, false);
-  }
-};
-
-/**
- * Handles the messages from the result queue.
- * Results and status will be appended to the single task that has run,
- * which then is written back to the initial overall job config.
- * @param {Object} msg The message
- */
-const handleResults = (msg) => {
-  try {
-    const job = JSON.parse(msg.content.toString());
-    log('Got a new task result...');
-    if (
-      job &&
-      job.nextTask &&
-      job.nextTask.task &&
-      job.nextTask.task.status === 'success'
-    ) {
-      // write back outputs to original job config
-      job.job[job.nextTask.idx] = job.nextTask.task;
-      // remove the succeeded job from the `nextTask` queue
-      delete job.nextTask;
-    }
-    log(`Sending job back to main worker queue ${workerQueue} ...`);
-    channel.sendToQueue(workerQueue, Buffer.from(JSON.stringify(job)), {
-      persistent: true
+    this.channel.consume(this.workerQueue, this.#createHandleNextTaskFun(), {
+      noAck: false
     });
-    channel.ack(msg);
-  } catch (e) {
-    log('Processing failed for result. ' + e);
-    // send to dead letter exchange
-    channel.nack(msg, false, false);
-  }
-};
 
-export default dispatcher;
+    this.channel.consume(this.resultQueue, this.#createHandleResultFun(), {
+      noAck: false
+    });
+  }
+
+  /**
+   * Creates a function that is executed when next task is available.
+   *
+   * Function needs to be created in this way to ensure it has access
+   * to the channel within its scope.
+   *
+   * @returns {Function} The function
+   */
+  #createHandleNextTaskFun() {
+
+    /**
+     * Determine the next task from job list and sends it to worker queue.
+     * If there is no task left, finish the execution with success.
+     * If an error is detected in a result message, we `nack` the job message which
+     * then will be forwarded to the dead letter exchange.
+     *
+     * @param {Object} msg The message
+     */
+    return async (msg) => {
+      let nextTaskEntry;
+
+      try {
+        const job = JSON.parse(msg.content.toString());
+        const chain = job.job;
+        log('Received a job configuration ...');
+
+        if (job.error) {
+          throw job.error;
+        }
+
+        // validate
+        if (!chain || chain.length < 1) {
+          throw 'Invalid arguments given' + job;
+        }
+
+        // create unique ID if necessary (on first run)
+        if (!job.id) {
+          job.id = randomUUID();
+        }
+
+        // find next task that has not run yet (status is not 'success')
+        nextTaskEntry = chain.find(
+          (task) => !(task.status && task.status === 'success')
+        );
+
+        if (nextTaskEntry) {
+          job.nextTask = {
+            task: nextTaskEntry,
+            idx: chain.findIndex((el) => el.id === nextTaskEntry.id)
+          };
+          log(`Sending the next task to queue ${nextTaskEntry.type} ...`);
+          this.channel.sendToQueue(
+            nextTaskEntry.type,
+            Buffer.from(JSON.stringify({ content: job })),
+            {
+              persistent: true
+            }
+          );
+        } else {
+          // overall job success
+          job.status = 'success';
+          const isRollBackJob = job?.job?.[0].type === 'rollback-handler';
+
+          if (isRollBackJob) {
+            log('Rollback job finished successfully: \n' + JSON.stringify(job, null, 2));
+          } else {
+            log('Job finished successfully: \n' + JSON.stringify(job, null, 2));
+          }
+        }
+
+        this.channel.ack(msg);
+      } catch (taskError) {
+        log('Processing failed for task. ' + taskError);
+        // send to dead letter exchange
+        this.channel.nack(msg, false, false);
+      }
+    };
+  }
+
+  /**
+   * Creates a function that is executed when result is ready.
+   *
+   * Function needs to be created in this way to ensure it has access
+   * to the channel within its scope.
+   *
+   * @returns {Function} The function
+   */
+  #createHandleResultFun() {
+
+    /**
+     * Handles the messages from the result queue.
+     * Results and status will be appended to the single task that has run,
+     * which then is written back to the initial overall job config.
+     *
+     * @param {Object} msg The message
+     */
+    return async (msg) => {
+      try {
+        const job = JSON.parse(msg.content.toString());
+        log('Got a new task result...');
+        if (job?.nextTask?.task?.status === 'success') {
+          // write back outputs to original job config
+          job.job[job.nextTask.idx] = job.nextTask.task;
+          // remove the succeeded job from the `nextTask` queue
+          delete job.nextTask;
+        }
+        log(`Sending job back to main worker queue ${this.workerQueue} ...`);
+        this.channel.sendToQueue(this.workerQueue, Buffer.from(JSON.stringify(job)), {
+          persistent: true
+        });
+        this.channel.ack(msg);
+      } catch (taskError) {
+        log('Processing failed for result. ' + taskError);
+        // send to dead letter exchange
+        this.channel.nack(msg, false, false);
+      }
+    }
+  }
+}
