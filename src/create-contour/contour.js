47215346cff3020abd68eb42fb7020806ed03dff
sdobbert <dobbert@terrestris.de>
2024-02-15 09:24:39 +0100

    make filename unique

--- src/create-contour/contour.js
+++ src/create-contour/contour.js
@@ -26,21 +26,21 @@ const execShellCommand = (cmd) => {
  * Relies on GRASS
  *
  * @param {String} inputPath The path of the GeoTIFF to convert
- * @param {String} fileName The name of the GeoTIFF
+ * @param {String} datasetTimestamp The timestamp of the GeoTIFF
  * @param {String} interval Elevation interval between contours
  *
  * @returns {Promise<String>} A Promise that resolves to the console output of the underlying GDAL process
  *
  * @throws If provided paths are invalid or conversion fails, an error is thrown
  */
-const createContourLines = async (inputPath, interval) => {
+const createContourLines = async (inputPath, datasetTimestamp, interval) => {
     // valdate inputPath
     if (! await fs.existsSync(inputPath)) {
         throw `Input file does not exist: ${inputPath}/`;
     }
 
     // build command for sub-process
-    const contourCmd = `gdal_contour -b 2 -a TEMP -i ${interval} -f "GeoJSON" /${inputPath} /tmp/output.geojson`;
+    const contourCmd = `gdal_contour -b 2 -a TEMP -i ${interval} -f "GeoJSON" /${inputPath} /tmp/output${datasetTimestamp}.geojson`;
 
     return await execShellCommand(contourCmd);
 }
