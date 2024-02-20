5325ec0ccdd884965de4c75022a273bf86c18037
sdobbert <dobbert@terrestris.de>
2024-02-20 15:01:42 +0100

    optimize geotiff

--- src/reclassify-geotiff/reclassifier.js
+++ src/reclassify-geotiff/reclassifier.js
@@ -64,7 +64,8 @@ const reclassifyGeoTiff = async (inputPath, outputPath, levels) => {
 
     // build command for sub-process
     //   -q: prevent non-error output
-    const reclassifyCmd = `gdal_calc.py -A ${inputPath} --calc="(A<=${levels[0]})*1 + ${cmdInput.join(' + ')} + (A>${lastLevel})*${levels.length + 1}" --outfile ${outputPath}`;
+    const reclassifyCmd = `gdal_calc.py -A ${inputPath} --calc="(A<=${levels[0]})*1 + ${cmdInput.join(' + ')} + (A>${lastLevel})*${levels.length + 1}" --outfile ${outputPath} |
+    gdal_translate ${inputPath} ${outputPath} -q -of COG -co COMPRESS="DEFLATE"`;
 
     return await execShellCommand(reclassifyCmd);
 }
