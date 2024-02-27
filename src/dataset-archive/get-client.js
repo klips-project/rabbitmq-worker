4d9826e93f8f2e3938651f40dc8b98666cf5e142
sdobbert <dobbert@terrestris.de>
2024-02-27 14:37:02 +0100

    add get-client function

---
+++ src/dataset-archive/get-client.js
@@ -0,0 +1,21 @@
+// Connect to databse
+import pkg from 'pg';
+const { Client } = pkg;
+
+const pgHost = process.env.POSTGRES_HOST;
+const pgPort = process.env.POSTGRES_PORT;
+const pgDatabase = process.env.POSTGRES_DB;
+const pgUser = process.env.POSTGRES_USER;
+const pgPassword = process.env.POSTGRES_PASSWORD;
+
+export const getClient = async () => {
+  const client = new Client({
+    host: pgHost,
+    port: pgPort,
+    user: pgUser,
+    password: pgPassword,
+    database: pgDatabase,
+  });
+  await client.connect();
+  return client;
+};
